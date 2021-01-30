0c37a69d6415445fc151d97a2c8f1a858bbeae17
kacperszyca <kacperszyca11@gmail.com>
2021-01-30 22:24:17 +0100

    added ability to search for films

--- src/pages/FetchMovies.jsx
+++ src/pages/FetchMovies.jsx
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function FetchMovies(pageNumber) {
+export default function FetchMovies(query, pageNumber, url) {
     const [movies, setMovies] = useState([])
     const [api_key] = React.useState("2d6ad82fcaf4bba35b7dac301e918cf2");
 
+    useEffect(() => {
+        setMovies([])
+    }, [query])
+
 
     useEffect(() => {
-
         let cancel
         axios({
             method: 'GET',
-            url: 'https://api.themoviedb.org/3/movie/popular',
-            params: {  api_key: api_key, page: pageNumber},
+            url: 'https://api.themoviedb.org/3/' +  url,
+            params: {  api_key: api_key, page: pageNumber, query: query},
             cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
             setMovies(prevMovies => {
-                console.log(res.data.results);
-                const newMovies = movies.concat(res.data.results);
-                return newMovies;
+                return [...new Set([...prevMovies, ...res.data.results])];
             });
-
         }).catch(e => {
             if (axios.isCancel(e)) return
         })
         return () => cancel()
-    }, [ pageNumber])
+    }, [ query, pageNumber, url])
 
     return {  movies }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,25 +10,31 @@ export default function MainPage() {
 
     const [query, setQuery] = useState('')
     const [pageNumber, setPageNumber] = useState(1)
+    const [url, setUrl] = React.useState("movie/popular");
 
     const {
         movies
-    } = FetchMovies( pageNumber)
+    } = FetchMovies(query, pageNumber, url)
 
 
     function handleSearch(e) {
         setQuery(e.target.value)
         setPageNumber(1)
+        if(e.target.value.length > 0){
+            setUrl("search/movie")
+        }else{
+            setUrl("movie/popular")
+        }
+
+        console.log()
     }
-
-
 
     return (
         <div className="outerMain">
             <input type="text" value={query} onChange={handleSearch}></input>
         <div className="innerMain">
 
-            <InfiniteScroll next={() => setPageNumber(pageNumber + 1)} hasMore={true}  dataLength={movies.length}>
+            <InfiniteScroll next={() => setPageNumber(pageNumber + 1)} hasMore={true}  dataLength={movies.length} loader={<h4>Loading...</h4>}>
                 <div className="movie-container">
                 {movies.length > 0 && movies.map((movie, index) =>
                    <Movie key={movie.id} data={movie}/>
